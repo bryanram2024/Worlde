b6453cd527a04c110b2a4f6e84df8cc7df4d5241
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2024-08-17 16:22:45 -0700

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -6,7 +6,7 @@ from colorama import Fore, Back, Style   # Imports 'colorama' library that adds 
 import openai                            # Imports 'openai' library to use ChatGPT to generate random word.
 
 # Openai API Key
-#openai.api_key = "sk-proj-h7653ybSG1getkhJPbjiT3BlbkFJbtisCzI5buaarYhP6VDf"
+openai.api_key = ""
 
 # Styling Elements
 green_h = Back.GREEN      # Highlights Green
